91af6cd25a9aa6caa96c5f65fade8234caa03c48
thewisenerd <thewisenerd@protonmail.com>
2025-05-08 02:43:34 +0530

    upstream from lb

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use async_trait::async_trait;
+use bytes::Bytes;
+use log::info;
 use pingora::prelude::*;
-use pingora::proxy::http_proxy_service;
 use pingora::proxy::ProxyHttp;
+use pingora::proxy::http_proxy_service;
 use std::net::ToSocketAddrs;
+use std::sync::Arc;
+use std::time::Duration;
 
-const IP4_ME_IP: &str = "1.1.1.1";
-const IP4_ME_PORT: u16 = 443;
-const IP4_ME_HOST: &str = "one.one.one.one";
+const PROXY_HOST: &str = "api.cloudflare.com";
 
-pub struct ProxyServer {
-    addr: std::net::SocketAddr,
-}
+pub struct ProxyServer(Arc<LoadBalancer<RoundRobin>>);
 
 #[async_trait]
 impl ProxyHttp for ProxyServer {
@@ -20,61 +20,46 @@ impl ProxyHttp for ProxyServer {
 
     async fn upstream_peer(
         &self,
-        session: &mut Session,
-        ctx: &mut Self::CTX,
+        _session: &mut Session,
+        _ctx: &mut Self::CTX,
     ) -> Result<Box<HttpPeer>> {
-        let peer = Box::new(HttpPeer::new(self.addr, true, IP4_ME_HOST.to_string()));
-        println!("upstream_peer: {:?}", peer);
+        let upstream = self.0.select(b"", 256).unwrap();
+        info!("upstream: {:?}", upstream);
+        let peer = Box::new(HttpPeer::new(upstream, true, PROXY_HOST.to_owned()));
         Ok(peer)
     }
 
     async fn upstream_request_filter(
         &self,
-        session: &mut Session,
+        _session: &mut Session,
         upstream_request: &mut RequestHeader,
-        ctx: &mut Self::CTX,
+        _ctx: &mut Self::CTX,
     ) -> Result<()>
     where
         Self::CTX: Send + Sync,
     {
-        upstream_request.insert_header("Host", IP4_ME_HOST.to_string())?;
+        upstream_request.insert_header("Host", PROXY_HOST.to_owned())?;
         Ok(())
-    }
-
-    async fn logging(
-        &self,
-        session: &mut Session,
-        _e: Option<&pingora::Error>,
-        ctx: &mut Self::CTX,
-    ) {
-        let response_code = session
-            .response_written()
-            .map_or(0, |resp| resp.status.as_u16());
-        // access log
-        println!(
-            "{} response code: {response_code}",
-            self.request_summary(session, ctx)
-        );
     }
 }
 
 fn main() {
+    env_logger::init();
+
     let mut server = Server::new(Some(Opt::default())).unwrap();
     server.bootstrap();
 
-    let mut http_proxy = http_proxy_service(
-        &server.configuration,
-        ProxyServer {
-            addr: (IP4_ME_IP, IP4_ME_PORT)
-                .to_socket_addrs()
-                .unwrap()
-                .next()
-                .unwrap(),
-        },
-    );
+    let addrs = (PROXY_HOST, 443).to_socket_addrs().unwrap();
+    for addr in addrs.clone() {
+        info!("upstream_addr: {:?}", addr);
+    }
+
+    let upstreams: LoadBalancer<RoundRobin> = LoadBalancer::try_from_iter(addrs).unwrap();
+
+    let mut http_proxy =
+        http_proxy_service(&server.configuration, ProxyServer(Arc::new(upstreams)));
     http_proxy.add_tcp("0.0.0.0:6191");
 
     server.add_service(http_proxy);
-    println!("starting server!");
-    server.run_forever();
+    server.run_forever()
 }
